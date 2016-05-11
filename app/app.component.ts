12990544ee3ff0a8c19105afcaab5b8dc5696201
praveen <nerkar.praveen@indiacansolutions.com>
2016-05-11 12:57:03 +0530

    all files added

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>  Angular2  App   running successfully...</h1>'
+})
+export class AppComponent { }
+
