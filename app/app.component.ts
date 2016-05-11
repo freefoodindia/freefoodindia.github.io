00aa2fe482aede532c881af8376eb549f4a9c681
praveen <nerkar.praveen@indiacansolutions.com>
2016-05-11 12:38:04 +0530

    removed all

--- app/app.component.ts
+++
@@ -1,9 +0,0 @@
-
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: '<h1>  Angular2  App   running successfully...</h1>'
-})
-export class AppComponent { }
-
